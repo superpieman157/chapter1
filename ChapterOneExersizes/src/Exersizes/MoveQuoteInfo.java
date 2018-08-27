6f214389965c059abfc4fa833b0de18990c8b03f
Giovanni Jacobsen <superpieman157@yahoo.com>
2018-08-27 09:46:29 -0500

    Added Info To Quote
    Added Info About My Favorite Movie Quote

---
+++ ChapterOneExersizes/src/Exersizes/MoveQuoteInfo.java
@@ -0,0 +1,13 @@
+package Exersizes;
+
+public class MoveQuoteInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Fish Are Freinds Not Food");
+        System.out.println("From The Movie Finding Nemo");
+        System.out.println("Said By The Great White Shark");
+        System.out.println("Movie Came Out In 2005");
+	}
+
+}
