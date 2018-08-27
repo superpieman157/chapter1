7a5d78d81bd6b0d6bd6b001357fcb502e671db0e
Giovanni Jacobsen <superpieman157@yahoo.com>
2018-08-27 09:47:14 -0500

    Added Qoute
    Added My Favorite Movie Quote

---
+++ ChapterOneExersizes/src/Exersizes/MovieQuote.java
@@ -0,0 +1,10 @@
+package Exersizes;
+
+public class MovieQuote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Fish Are Freinds Not Food");
+	}
+
+}
