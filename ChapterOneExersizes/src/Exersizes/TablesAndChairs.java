5fbfd8e7eca84d676ed560e7d523b7adfd3e7e43
Giovanni Jacobsen <superpieman157@yahoo.com>
2018-08-27 09:14:18 -0500

    Added Tables and chairs
    Added some nice tables and chairs

--- ChapterOneExersizes/src/Exersizes/TablesAndChairs.java
+++ ChapterOneExersizes/src/Exersizes/TablesAndChairs.java
@@ -5,6 +5,16 @@ public class TablesAndChairs {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		System.out.println("X                    X");
+		System.out.println("X                    X");
+		System.out.println("X      XXXXXXXX      X");
+		System.out.println("X      X      X      X");
+		System.out.println("X      X      X      X");
+		System.out.println("XXXXX  X      X  XXXXX");
+		System.out.println("X   X  X      X  X   X");
+		System.out.println("X   X  X      X  X   X");
+		System.out.println("X   X  X      X  X   X");
 	}
 
 }
