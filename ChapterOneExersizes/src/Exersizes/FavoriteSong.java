7c7125f93268ba13b5ee2b90cc841ae22ceee23f
Giovanni Jacobsen <superpieman157@yahoo.com>
2018-08-27 09:45:55 -0500

    Added Lines From Song
    Added Lines From My Favorite Song

---
+++ ChapterOneExersizes/src/Exersizes/FavoriteSong.java
@@ -0,0 +1,13 @@
+package Exersizes;
+
+public class FavoriteSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("There's A Star Man Waiting In The Sky");
+        System.out.println("He'd Like To Come And Meet Us But He Thinks He'd Blow Our Minds");
+        System.out.println("He Told Us Not To Blow It Cuz He Knows It's All Worth While");
+        System.out.println("Let The Children Use It Let The Children Lose It Let the Children Boogie");
+	}
+
+}
