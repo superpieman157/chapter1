15177c4f87cec231043ce7d17af7ffe159885c16
Giovanni Jacobsen <superpieman157@yahoo.com>
2018-08-28 09:15:18 -0500

    Random Number
    Made  A Random Number Generator

---
+++ ChapterOneExersizes/src/Exersizes/RandomNumber.java
@@ -0,0 +1,10 @@
+package Exersizes;
+import javax.swing.JOptionPane;
+public class RandomNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+JOptionPane.showMessageDialog(null,"The Number is "+(1 + (int)(Math.random() * 100000)));
+	}
+
+}
