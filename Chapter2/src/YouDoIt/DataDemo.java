127b4b0c1aca8f2a43b83c8a559ec79da0f54896
Giovanni Jacobsen <superpieman157@yahoo.com>
2018-08-29 09:40:41 -0500

    Added stuff
    Added ints and longs with other stuff too

--- Chapter2/src/YouDoIt/DataDemo.java
+++ Chapter2/src/YouDoIt/DataDemo.java
@@ -4,7 +4,11 @@ public class DataDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        int aWholeNumber = 315;
+        final int STATES_IN_US = 50;
+        System.out.println("The Number Is " + aWholeNumber);
+        System.out.println(aWholeNumber);
+        System.out.println("The number of states is " + STATES_IN_US + " Or three if your doing states of matter");
 	}
 
 }
