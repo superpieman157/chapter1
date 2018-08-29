3e0b771518064add7305d888fe5c32a4cdd2a108
Giovanni Jacobsen <superpieman157@yahoo.com>
2018-08-29 08:34:22 -0500

    Added lines
    Added extra popups for a more fun expirience

--- ChapterOneExersizes/src/Exersizes/RandomNumber.java
+++ ChapterOneExersizes/src/Exersizes/RandomNumber.java
@@ -4,7 +4,9 @@ public class RandomNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Guess a number between 1 and 100000");
 JOptionPane.showMessageDialog(null,"The Number is "+(1 + (int)(Math.random() * 100000)));
+		JOptionPane.showMessageDialog(null, "What did you guess?");
 	}
 
 }
