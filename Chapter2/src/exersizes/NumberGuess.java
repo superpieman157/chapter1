d56f13d544899163c66a3a835644fd9bfc4a1f99
Giovanni Jacobsen <superpieman157@yahoo.com>
2018-09-05 09:31:13 -0500

    RandGuess
    Made A Number Guessing Game

---
+++ Chapter2/src/exersizes/NumberGuess.java
@@ -0,0 +1,23 @@
+package exersizes;
+import java.util.Scanner; 
+public class NumberGuess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner input = new Scanner(System.in);
+        
+        int Guess;
+		
+		System.out.println("Guess A Number Between 1 And 5");
+		Guess = input.nextInt();
+        
+        int random = 1 + (int)(Math.random() * 5);
+        
+        int Difference = random - Guess;
+        
+        System.out.println(random);
+        
+        System.out.println("The Difference Between Your Guess And The Number Was " + Difference);
+	}
+
+}
